6b8aa6165f1a9aed84dc34f5c98a4dc3f2e0d1f2
ilya928278 <8tilya@gmail.com>
2024-05-09 12:15:23 +0200

    Commit

--- Console-program-with-objects/Program.cs
+++ Console-program-with-objects/Program.cs
@@ -7,7 +7,31 @@ namespace hillel2
 {
     public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; init; }
+        public Gender UserGender { get; set; }
+        public enum Gender
+        {
+            Male,
+            Female,
+            Unknown
+        }
 
+        public User()
+        {
+
+        }
+
+        public User(int age)
+        {
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Hi, my name is {FirstName} and last name {LastName}. I am {20} years old. I am {UserGender}".ToString();
+        }
     }
 
 
