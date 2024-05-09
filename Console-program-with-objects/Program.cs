8f8dad02ad4c7811d344a169383c6e58068593a5
ilya928278 <8tilya@gmail.com>
2024-05-09 12:15:42 +0200

    Commit

--- Console-program-with-objects/Program.cs
+++ Console-program-with-objects/Program.cs
@@ -34,5 +34,31 @@ namespace hillel2
         }
     }
 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            User user1 = new User();
+            user1.UserGender = User.Gender.Unknown;
 
+            Regex regex = new Regex(@"^[a-zA-Z]+$");
+
+            Console.Write("First name: ");
+            user1.FirstName = (Console.ReadLine());
+
+            Console.Write("Last name: ");
+            user1.LastName = (Console.ReadLine());
+
+            if (regex.IsMatch(user1.FirstName) && regex.IsMatch(user1.LastName))
+            {
+                Console.WriteLine($"FirstName: {user1.FirstName} --- LastName: {user1.LastName}");
+            }
+            else
+            {
+                Console.WriteLine("Wrong name");
+            }
+
+            Console.WriteLine(user1.ToString());
+        }
+    }
 }
